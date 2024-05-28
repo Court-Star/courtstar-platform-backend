25b2cb118d1b922083b3ba82e7d43ad9e4a27ea4
thanhntse <nguyenthaithanh07@gmail.com>
2024-05-28 22:28:19 +0700

    feat: configure cross origin

--- src/main/java/com/example/courtstar/controller/AccountController.java
+++ src/main/java/com/example/courtstar/controller/AccountController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/account")
 public class AccountController {
