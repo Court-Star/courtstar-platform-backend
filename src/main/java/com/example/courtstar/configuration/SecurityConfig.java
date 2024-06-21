7e40e52f8830aabd5b7960af7eb53c336ff67d89
thanhntse <nguyenthaithanh07@gmail.com>
2024-06-21 12:46:56 +0700

    feat: order status req

--- src/main/java/com/example/courtstar/configuration/SecurityConfig.java
+++ src/main/java/com/example/courtstar/configuration/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     private CustomJwtDecoder customJwtDecoder;
     private final String[] PUBLIC_URLS_POST = {"/account","/account/partner","/auth/token","/auth/introspect"
             ,"/auth/logout","/auth/refresh","/account/reset-password","/booking","/payment/order-status"
-            ,"/payment/donate-callback","/payment/booking-callback"};
+            ,"/payment/donate-callback","/payment/booking-callback","/payment/order-info"};
     private final String[] PUBLIC_URLS_GET = {"/centre/getAllCentreActive", "/centre/getCentre/{id}","/court/{centreId}/{courtNo}"
             ,"/feedback/{id}"};
     private final String[] PUBLIC_URLS_PUT = {"/account/regenerate-otp"};
