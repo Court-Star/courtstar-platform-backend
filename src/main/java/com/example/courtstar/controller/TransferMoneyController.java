0a08ee68e99e56d3577a234a7fb28125cfb5601a
thanhntse <nguyenthaithanh07@gmail.com>
2024-07-01 11:26:17 +0700

    feat: add top up controller and refactor transfer controller

--- src/main/java/com/example/courtstar/controller/TransferMoneyController.java
+++ src/main/java/com/example/courtstar/controller/TransferMoneyController.java
@@ -2,9 +2,9 @@ package com.example.courtstar.controller;
 
 import com.example.courtstar.dto.request.ApiResponse;
 import com.example.courtstar.dto.request.AuthWithdrawalOrderRequest;
-import com.example.courtstar.dto.request.TranferMoneyRequest;
+import com.example.courtstar.dto.request.TransferMoneyRequest;
 import com.example.courtstar.dto.response.AuthWithdrawalOrderResponse;
-import com.example.courtstar.dto.response.TranferMoneyReponse;
+import com.example.courtstar.dto.response.TransferMoneyReponse;
 import com.example.courtstar.entity.TransferMoney;
 import com.example.courtstar.services.TransferMoneyService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,39 +12,40 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/tranfermoney")
-public class TranferMoneyController {
+@RequestMapping("/transfer-money")
+public class TransferMoneyController {
     @Autowired
     TransferMoneyService transferMoneyService;
-    @PostMapping("/createWithdrawalOrder/{idManagerCentre}")
-    public ApiResponse<TranferMoneyReponse> createTranferMoney(@PathVariable int idManagerCentre,@RequestBody TranferMoneyRequest request) {
-        return  ApiResponse.<TranferMoneyReponse>builder()
-                .data(transferMoneyService.createTranferMoney(idManagerCentre,request)).build();
+    @PostMapping("/create-withdrawal/{id}")
+    public ApiResponse<TransferMoneyReponse> createTransferMoney(@PathVariable int id, @RequestBody TransferMoneyRequest request) {
+        return  ApiResponse.<TransferMoneyReponse>builder()
+                .data(transferMoneyService.createTransferMoney(id,request)).build();
     }
-    @PostMapping("/authenticateWithdrawalOrder/{id}")
+    @PostMapping("/authenticate-withdrawal-order/{id}")
     public ApiResponse<AuthWithdrawalOrderResponse> authenticateWithdrawalOrder(@PathVariable int id, @RequestBody AuthWithdrawalOrderRequest request){
         return ApiResponse.<AuthWithdrawalOrderResponse>builder()
-                .data(transferMoneyService.authenticateTranferMoney(request,id))
+                .data(transferMoneyService.authenticateTransferMoney(request,id))
                 .build();
     }
-    @GetMapping("/getAllWithdrawalOrder")
+    @GetMapping("/all")
     public ApiResponse<List<TransferMoney>> getAllWithdrawalOrder(){
         return ApiResponse.<List<TransferMoney>>builder()
-                .data(transferMoneyService.getAllTranferMoney())
+                .data(transferMoneyService.getAllTransferMoney())
                 .build();
     }
 
     @GetMapping("/getAllSuccess")
-    public ApiResponse<List<TransferMoney>> getListTranferSuccess(){
+    public ApiResponse<List<TransferMoney>> getListTransferSuccess(){
         return ApiResponse.<List<TransferMoney>>builder()
-                .data(transferMoneyService.getListTranferSuccess())
+                .data(transferMoneyService.getListTransferSuccess())
                 .build();
     }
     @GetMapping("/getAllNotSuccess")
-    public ApiResponse<List<TransferMoney>> getListTranferNotSuccess(){
+    public ApiResponse<List<TransferMoney>> getListTransferNotSuccess(){
         return ApiResponse.<List<TransferMoney>>builder()
-                .data(transferMoneyService.getListTranferNotSuccess())
+                .data(transferMoneyService.getListTransferNotSuccess())
                 .build();
     }
 }
--- src/main/java/com/example/courtstar/mapper/TransferMoneyMapper.java
+++ src/main/java/com/example/courtstar/mapper/TransferMoneyMapper.java
@@ -1,14 +1,14 @@
 package com.example.courtstar.mapper;
 
-import com.example.courtstar.dto.request.TranferMoneyRequest;
+import com.example.courtstar.dto.request.TransferMoneyRequest;
 import com.example.courtstar.dto.response.AuthWithdrawalOrderResponse;
-import com.example.courtstar.dto.response.TranferMoneyReponse;
+import com.example.courtstar.dto.response.TransferMoneyReponse;
 import com.example.courtstar.entity.TransferMoney;
 import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface TransferMoneyMapper {
-    TransferMoney toTransferMoney(TranferMoneyRequest request);
-    TranferMoneyReponse toTranferMoneyReponse(TransferMoney transferMoney);
+    TransferMoney toTransferMoney(TransferMoneyRequest request);
+    TransferMoneyReponse toTranferMoneyReponse(TransferMoney transferMoney);
     AuthWithdrawalOrderResponse toAuthWithdrawalOrderResponse(TransferMoney transferMoney);
 
 }
