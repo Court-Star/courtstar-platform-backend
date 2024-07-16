5178a6da0a40d9270262e72a8d341cebff4c6380
thanhntse <nguyenthaithanh07@gmail.com>
2024-07-16 14:33:17 +0700

    refactor: vn pay

--- src/main/java/com/example/courtstar/controller/VnpayController/CallbackPaymentController.java
+++ src/main/java/com/example/courtstar/controller/VnpayController/CallbackPaymentController.java
@@ -15,16 +15,14 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
+@CrossOrigin(origins = {"http://localhost:3000", "https://courtstar-platform-frontend.vercel.app/"})
 @RestController
 @RequestMapping("/payment-vn-pay")
 
@@ -46,6 +44,8 @@ public class CallbackPaymentController {
     @GetMapping("/callback")
     public ResponseEntity<Map<String, Object>> doCallBack(@RequestParam Map<String, Object> callBackInfo) throws JSONException, MessagingException, IOException, WriterException {
         System.out.println(callBackInfo);
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("status", "0");
         String vnp_ResponseCode = (String) callBackInfo.get("vnp_ResponseCode");
         if("00".equals(vnp_ResponseCode)){
             String dataStr=(String) callBackInfo.get("vnp_OrderInfo");
@@ -57,35 +57,35 @@ public class CallbackPaymentController {
             int paymentId = jsonObject.getInt("paymentId");
             int centreId = jsonObject.getInt("centreId");
             Payment payment = paymentRepository.findById(paymentId).orElseThrow(null);
-            payment.setStatus(true);
-            payment.setVnpTransId((String) callBackInfo.get("vnp_TransactionStatus"));
-            paymentRepository.save(payment);
 
-            BookingSchedule bookingSchedule = bookingScheduleRepository.findById(bookingId).orElseThrow(null);
-            bookingSchedule.setSuccess(true);
-            bookingSchedule.getBookingDetails()
-                    .forEach(detail -> detail.setStatus(true));
-            bookingScheduleRepository.save(bookingSchedule);
+            resultMap.put("appTransId", payment.getTransactionCode());
+            resultMap.put("status", "1");
 
-            qrCodeService.generateQrCode(bookingId, payment.getTransactionCode());
+            if (!payment.isStatus()) {
+                payment.setStatus(true);
+                payment.setVnpTransId((String) callBackInfo.get("vnp_TransactionNo"));
+                paymentRepository.save(payment);
+
+                BookingSchedule bookingSchedule = bookingScheduleRepository.findById(bookingId).orElseThrow(null);
+                bookingSchedule.setSuccess(true);
+                bookingSchedule.getBookingDetails()
+                        .forEach(detail -> detail.setStatus(true));
+                bookingScheduleRepository.save(bookingSchedule);
+
+                qrCodeService.generateQrCode(bookingId, payment.getTransactionCode());
 
 
-            Centre centre =centreRepository.findById(centreId).orElseThrow(()->new AppException(ErrorCode.NOT_FOUND_CENTRE));
-            Double payMoney = centre.getRevenue()+(long)callBackInfo.get("vnp_Amount");
-            centre.setRevenue(payMoney);
-            centreRepository.save(centre);
+                Centre centre =centreRepository.findById(centreId).orElseThrow(()->new AppException(ErrorCode.NOT_FOUND_CENTRE));
+                Double payMoney = centre.getRevenue()+Long.parseLong((String)callBackInfo.get("vnp_Amount"));
+                centre.setRevenue(payMoney);
+                centreRepository.save(centre);
 
-            Double totalPayMoney = centre.getManager().getCurrentBalance()+(long)callBackInfo.get("vnp_Amount")*0.95;
-            centre.getManager().setCurrentBalance(totalPayMoney);
-            centreManagerRepository.save(centre.getManager());
-            // them duong dan return den
-
-        }else {
-            System.out.println("hi");
-
-            // them duong dan return den
+                Double totalPayMoney = centre.getManager().getCurrentBalance()+Long.parseLong((String)callBackInfo.get("vnp_Amount"))*0.95;
+                centre.getManager().setCurrentBalance(totalPayMoney);
+                centreManagerRepository.save(centre.getManager());
+            }
         }
 
-        return new ResponseEntity<>(new HashMap<>(), HttpStatus.OK);
+        return new ResponseEntity<>(resultMap, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/courtstar/controller/VnpayController/OrderPaymentController.java
+++ src/main/java/com/example/courtstar/controller/VnpayController/OrderPaymentController.java
@@ -7,14 +7,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:3000", "https://courtstar-platform-frontend.vercel.app/"})
 @RestController
 @RequestMapping("/payment-vn-pay")
 public class OrderPaymentController {
